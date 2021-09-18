22c31e29efe659a37c9de6a4e99c356fb39b3b8f
danhdzr <josephdzr@gmail.com>
2021-09-18 12:01:57 -0500

    Add addEntries, listEntries

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int authenticate()
   {
     printf("No master password found, please enter one (max 25 characters)\n");
     FILE *ofp;
-    ofp = fopen("pass.txt", "w");
+    ofp = fopen("pass.txt", "wb");
     scanf("%s", pass);
     strcpy(passNode->pass, pass);
     fwrite(passNode, sizeof(passNode), 1, ofp);
@@ -56,8 +56,99 @@ int authenticate()
   return 0;
 }
 
+void createEntry()
+{
+  Entry *e;
+  FILE *fp;
+  char p[140];
+
+  e = (Entry *)malloc(sizeof(Entry));
+  fp = fopen("entries.txt", "wb");
+
+  //Obligatory field
+
+  printf("Enter name\n");
+  scanf("%s", p);
+  strcpy(e->name, p);
+  fflush(stdin);
+
+  //Obligatory field
+
+  printf("Enter username/email\n");
+  scanf("%s", p);
+  strcpy(e->email, p);
+  fflush(stdin);
+
+  //Obligatory field
+
+  printf("Enter your password\n");
+  scanf("%s", p);
+  fflush(stdin);
+  strcpy(e->password, p);
+
+  //Must fix optinal sections
+  printf("Enter a description (Press Enter to skip)\n");
+  fgets(p, 20, stdin);
+  //printf("Description: %s\n", p);
+  strcpy(e->description, p);
+
+  printf("Enter a url (Press Enter to skip)\n");
+  fgets(p, 20, stdin);
+  strcpy(e->url, p);
+
+  fwrite(e, sizeof(Entry), 1, fp);
+
+  fclose(fp);
+}
+
+void listEntries()
+{
+  Entry *e;
+  e = (Entry *)malloc(sizeof(Entry));
+
+  FILE *fp;
+  fp = fopen("entries.txt", "rb");
+
+  if (fp != NULL)
+  {
+    while (fread(e, sizeof(Entry), 1, fp) == 1)
+    {
+
+      fread(e, sizeof(Entry), 1, fp);
+      printf("Username: %s Username/email: %s Password: %s Description: %s Url: %s \n", e->name, e->email, e->password, e->description, e->url);
+    }
+  }
+  fclose(fp);
+}
+
 int main()
 {
   int authRes = authenticate();
+  int option;
+
+  do
+  {
+    printf("1. Create entry\n");
+    printf("2. Delete entry\n");
+    printf("3. View all entries\n");
+    printf("4. Select entry\n");
+    printf("0. Exit Program\n");
+
+    printf("Select an option: ");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+      createEntry();
+      break;
+    case 2:
+      //delete()
+      break;
+    case 3:
+      listEntries();
+    }
+
+  } while (option != 0);
   return 0;
 }
